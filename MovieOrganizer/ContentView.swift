c1b67075be7b2519663814448aebd0d5fdd28d30
WalkLightly <runnerknight@gmail.com>
2026-06-19 18:17:52 -0600

    Small change to see if it works

--- MovieOrganizer/ContentView.swift
+++ MovieOrganizer/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
             Button(action: {
                         print("Button Tapped!")
                     }) {
-                        Text("Button")
+                        Text("Small Change")
                             .font(.system(size: 22, weight: .bold))
                             .foregroundColor(.black) // Match text to border
                             .padding(.vertical, 10)
